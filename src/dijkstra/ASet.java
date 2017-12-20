8961a5a2c2858c789ae666653556d269b2ef3c37
Raphael REME <raphael.reme@telecom-paristech.fr>
2017-12-20 14:14:45 +0100

    Rajout des implementations generales pour dijkstra

---
+++ src/dijkstra/ASet.java
@@ -0,0 +1,20 @@
+package dijkstra;
+
+import java.util.HashSet;
+
+import interfaces.ASetInterface;
+import interfaces.VertexInterface;
+
+public class ASet implements ASetInterface {
+	
+	private HashSet<VertexInterface> set = new HashSet<VertexInterface>();
+
+	public boolean isIn(VertexInterface v) {
+		return set.contains(v);
+	}
+
+	public void add(VertexInterface v) {
+		set.add(v);
+	}
+	
+}
---
+++ src/dijkstra/Previous.java
@@ -0,0 +1,20 @@
+package dijkstra;
+
+import java.util.Hashtable;
+
+import interfaces.PreviousInterface;
+import interfaces.VertexInterface;
+
+public class Previous implements PreviousInterface {
+	
+	Hashtable<VertexInterface,VertexInterface> t = new Hashtable<VertexInterface,VertexInterface>();
+
+	public VertexInterface getPrev(VertexInterface v) {
+		return t.get(v);
+	}
+
+	public void setPrev(VertexInterface v, VertexInterface prev) {
+		t.put(v, prev);
+	}
+
+}
