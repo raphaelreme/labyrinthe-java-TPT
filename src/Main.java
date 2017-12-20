7e6a0e5ca7e2fa21161bad89c371526acb4e4ad8
Lucas Ohnheiser <lucasohnheiser@gmail.com>
2017-12-20 15:06:44 +0100

    Commentaire encodage

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
-import graphique.Frame;
+/*
+Attention, ce projet utilise l'encodage ISO-8859-1
+ */
 
+import graphique.Frame;
 
 public class Main {
 
 	public static void main(String[] args){
 	//Branche Lucas
 	Frame f = new Frame();
+
+
 	}
 
 }
