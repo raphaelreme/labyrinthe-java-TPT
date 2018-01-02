4efd3766f06376dce2ba54e7a937e1a3cfdc5ce8
Raphael REME <raphael.reme@telecom-paristech.fr>
2018-01-02 17:20:11 +0100

    essai d'un JFileChooser avec la menubar

--- src/graphique/Frame.java
+++ src/graphique/Frame.java
@@ -2,13 +2,18 @@ package graphique;
 
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import modele.MazeModel;
 
@@ -29,6 +34,20 @@ public class Frame extends JFrame implements Observer {
 		contentPanel = new Panel(this);
 		JMenu m = new JMenu("File");
 		m.add(new JMenuItem("Quit"));
+		JMenuItem op = new JMenuItem("Open");
+		op.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				JFileChooser fc = new JFileChooser();
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("Texte","txt");
+				fc.setCurrentDirectory(new File("file"));
+				fc.setFileFilter(filter);
+				int returnVal = fc.showOpenDialog(Frame.this);
+				File file = fc.getSelectedFile();
+				System.out.println(file.getPath()+" : "+returnVal);
+				model.changeFile(file.getPath());
+			}
+		});
+		m.add(op);
 		JMenuBar mb = new JMenuBar();
 		mb.add(m);
 		mb.setBackground(Color.gray);
